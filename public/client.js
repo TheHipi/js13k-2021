83ba1131d963dcc323479fd090ef7de0c941b778
gradiuscypher <gradiuscypher@gmail.com>
2021-08-14 23:36:48 -0700

    some example code with a chatroom

--- public/client.js
+++ public/client.js
@@ -2,108 +2,31 @@
 
 (function () {
 
-    let socket, //Socket.IO client
-        buttons, //Button elements
-        message, //Message element
-        score, //Score element
-        points = { //Game points
-            draw: 0,
-            win: 0,
-            lose: 0
-        };
-
-    /**
-     * Disable all button
-     */
-    function disableButtons() {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].setAttribute("disabled", "disabled");
-        }
-    }
-
-    /**
-     * Enable all button
-     */
-    function enableButtons() {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].removeAttribute("disabled");
-        }
-    }
-
-    /**
-     * Set message text
-     * @param {string} text
-     */
-    function setMessage(text) {
-        message.innerHTML = text;
-    }
-
-    /**
-     * Set score text
-     * @param {string} text
-     */
-    function displayScore(text) {
-        score.innerHTML = [
-            "<h2>" + text + "</h2>",
-            "Won: " + points.win,
-            "Lost: " + points.lose,
-            "Draw: " + points.draw
-        ].join("<br>");
-    }
-
+    let socket //Socket.IO client
     /**
      * Binde Socket.IO and button events
      */
     function bind() {
-
-        socket.on("start", () => {
-            enableButtons();
-            setMessage("Round " + (points.win + points.lose + points.draw + 1));
-        });
-
-        socket.on("win", () => {
-            points.win++;
-            displayScore("You win!");
-        });
-
-        socket.on("lose", () => {
-            points.lose++;
-            displayScore("You lose!");
-        });
-
-        socket.on("draw", () => {
-            points.draw++;
-            displayScore("Draw!");
-        });
-
-        socket.on("end", () => {
-            disableButtons();
-            setMessage("Waiting for opponent...");
-        });
-
         socket.on("connect", () => {
-            disableButtons();
-            setMessage("Waiting for opponent...");
+            console.log("connected")
         });
 
         socket.on("disconnect", () => {
-            disableButtons();
-            setMessage("Connection lost!");
+            console.log("disconnected")
         });
 
         socket.on("error", () => {
-            disableButtons();
-            setMessage("Connection error!");
+            console.log("error")
         });
 
-        for (let i = 0; i < buttons.length; i++) {
-            ((button, guess) => {
-                button.addEventListener("click", function (e) {
-                    disableButtons();
-                    socket.emit("guess", guess);
-                }, false);
-            })(buttons[i], i + 1);
-        }
+        socket.on("chat", (message) => {
+            new_chat(message);
+        });
+    }
+
+    function new_chat(message) {
+        document.getElementById("chat").value += "\n" + message;
+        document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
     }
 
     /**
@@ -111,10 +34,11 @@
      */
     function init() {
         socket = io({ upgrade: false, transports: ["websocket"] });
-        buttons = document.getElementsByTagName("button");
-        message = document.getElementById("message");
-        score = document.getElementById("score");
-        disableButtons();
+        var send = document.getElementById("send");
+        send.addEventListener("click", function (e) {
+            socket.emit("send", socket.id, document.getElementById("msgbar").value);
+            document.getElementById("msgbar").value = '';
+        })
         bind();
     }
 
